cabcd94b10d70e5a92b50c84a2e98c794f57be41
Prathmesh00007 <prathmeshranade23@gmail.com>
2026-04-06 00:30:35 +0530

    razrpay

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,6 @@ if (process.env.NODE_ENV_CUSTOM === "production") {
 }
 
 //port
-app.listen(5000, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(8000, () => {
+  console.log("listening on 8000");
 });
